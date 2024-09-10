a825eb6368c1e7aa8b3ad9bf04a39d940df25891
ratnesh003 <ratneshpasi03@gmail.com>
2024-09-10 11:52:09 +0530

    code logic adjustment

--- AI Assignments/assignment5Q2.cpp
+++ AI Assignments/assignment5Q2.cpp
@@ -1,25 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int gcd(int a, int b)
-{
-    if (b==0)
-       return a;
-    return gcd(b, a%b);
-}
-
 vector<pair<int, int>> pour(int fromCap, int toCap, int d)
 {
     int from = fromCap;
     int to = 0;
 
-    vector<pair<int, int>> sequen;
+    vector<pair<int, int>> sequence;
 
     pair<int, int> step;
     step.first = from;
     step.second = to;
 
-    sequen.push_back(step);
+    sequence.push_back(step);
 
     while (from != d && to != d)
     {
@@ -31,7 +24,7 @@ vector<pair<int, int>> pour(int fromCap, int toCap, int d)
         step.first = from;
         step.second = to;
 
-        sequen.push_back(step);
+        sequence.push_back(step);
 
         if (from == d || to == d)
             break;
@@ -47,28 +40,14 @@ vector<pair<int, int>> pour(int fromCap, int toCap, int d)
         }
     }
 
-    return sequen;
-}
-
-vector<pair<int, int>> sequence(int m, int n, int d)
-{
-    if (m > n)
-        swap(m, n);
-
-    // if (d > n)
-    //     return -1;
-
-    // if ((d % gcd(n,m)) != 0)
-    //     return -1;
-
-    return pour(n,m,d);
+    return sequence;
 }
 
 int main()
 {
     int n = 3, m = 5, d = 4;
     vector<pair<int, int>> ans;
-    ans = sequence(m, n, d);
+    ans = pour(m, n, d);
 
     for(int i = 0; i<ans.size(); i++) {
         cout<<" -> ("<<ans[i].first<<","<<ans[i].second<<")";
