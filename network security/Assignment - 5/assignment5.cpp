94b561187761107fabbdd1f82ae3ea337cd37277
ratnesh003 <ratneshpasi03@gmail.com>
2024-10-24 16:21:08 +0530

    hill cipher updated

--- network security/Assignment - 5/assignment5.cpp
+++ network security/Assignment - 5/assignment5.cpp
@@ -6,14 +6,19 @@ const int MOD = 26;
 int KEYMATRIX[N][N];
 int KEYMATRIXINVERSE[N][N];
 
-void getCofactor(vector<vector<int> >& temp, int p, int q, int n) {
+void getCofactor(vector<vector<int>> &temp, int p, int q, int n)
+{
     int i = 0, j = 0;
-    for (int row = 0; row < n; row++) {
-        for (int col = 0; col < n; col++) {
-            if (row != p && col != q) {
+    for (int row = 0; row < n; row++)
+    {
+        for (int col = 0; col < n; col++)
+        {
+            if (row != p && col != q)
+            {
                 temp[i][j++] = KEYMATRIX[row][col];
 
-                if (j == n - 1) {
+                if (j == n - 1)
+                {
                     j = 0;
                     i++;
                 }
@@ -22,15 +27,35 @@ void getCofactor(vector<vector<int> >& temp, int p, int q, int n) {
     }
 }
 
-void adjoint() {
-    if (N == 1) {
+int determinant(int n)
+{
+    int D = 0;
+    if (n == 1)
+        return KEYMATRIX[0][0];
+    vector<vector<int>> temp(N, vector<int>(N));
+    int sign = 1;
+    for (int f = 0; f < n; f++)
+    {
+        getCofactor(temp, 0, f, n);
+        D += sign * KEYMATRIX[0][f] * determinant(n - 1);
+        sign = -sign;
+    }
+    return D;
+}
+
+void adjoint()
+{
+    if (N == 1)
+    {
         KEYMATRIXINVERSE[0][0] = 1;
         return;
     }
     int sign = 1;
-    vector<vector<int> > temp(N, vector<int>(N));
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+    vector<vector<int>> temp(N, vector<int>(N));
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
             getCofactor(temp, i, j, N);
 
             sign = ((i + j) % 2 == 0) ? 1 : -1;
@@ -40,41 +65,41 @@ void adjoint() {
     }
 }
 
-int determinant(int n) {
-    int D = 0;
-    if (n == 1)
-        return KEYMATRIX[0][0];
-    vector<vector<int> > temp(N, vector<int>(N));
-    int sign = 1;
-    for (int f = 0; f < n; f++) {
-        getCofactor(temp, 0, f, n);
-        D += sign * KEYMATRIX[0][f] * determinant(n - 1);
-        sign = -sign;
-    }
-    return D;
-}
-
-int modInverse(int A) {
+int modInverse(int A)
+{
     for (int X = 1; X < MOD; X++)
         if (((A % MOD) * (X % MOD)) % MOD == 1)
             return X;
 }
 
+int keyMatricGeneration()
+{
+    cout << "Enter the KEY of 3X3 :\n"
+         << endl;
 
-int keyMatricGeneration() {
-    cout<<"Enter the KEY of 3X3 :\n"<<endl;
-
-    for(int i =0; i<N; i++) {
-        for(int j=0; j<N; j++) {
-            cout<<"\t enter "<<i<<", "<<j<<" value of KEY : ";
-            cin>>KEYMATRIX[i][j];
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << "\t enter " << i << ", " << j << " value of KEY : ";
+            cin >> KEYMATRIX[i][j];
         }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            cout << KEYMATRIX[i][j] << " ";
+        }
+        cout << endl;
     }
 
     int check = determinant(N);
 
-    if(check == 0 ) {
-        cout<<"KEY cannot be used for encryption";
+    if (check == 0)
+    {
+        cout << "KEY cannot be used for encryption";
         return 1;
     }
 
@@ -82,27 +107,104 @@ int keyMatricGeneration() {
 
     adjoint();
 
-    for(int i =0; i<N; i++) {
-        for(int j=0; j<N; j++) {
-            KEYMATRIXINVERSE[i][j] = (KEYMATRIXINVERSE[i][j]*detInverse) % MOD;
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            KEYMATRIXINVERSE[i][j] = (KEYMATRIXINVERSE[i][j] * detInverse) % MOD;
         }
     }
 
     return 0;
 }
 
-string hillCipherEncryption(string plaintext) {
+void encrypt(int cipherMatrix[][1], int messageVector[][1])
+{
+    int x, i, j;
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 1; j++)
+        {
+            cipherMatrix[i][j] = 0;
+
+            for (x = 0; x < 3; x++)
+            {
+                cipherMatrix[i][j] += KEYMATRIX[i][x] * messageVector[x][j];
+            }
+
+            cipherMatrix[i][j] = cipherMatrix[i][j] % 26;
+        }
+    }
+}
+
+void decrypt(int messageMatrix[][1], int cipherVector[][1])
+{
+    int x, i, j;
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 1; j++)
+        {
+            messageMatrix[i][j] = 0;
+
+            for (x = 0; x < 3; x++)
+            {
+                messageMatrix[i][j] += KEYMATRIXINVERSE[i][x] * cipherVector[x][j];
+            }
+
+            messageMatrix[i][j] = messageMatrix[i][j] % 26;
+        }
+    }
+}
+
+string hillCipherEncryption(string plaintext)
+{
     string cipherText;
-    while(plaintext.length()%3 != 0) {
+    while (plaintext.length() % 3 != 0)
+    {
         plaintext.append("Z");
     }
 
-    
+    for (int i = 0; i < plaintext.length() / 3; i++)
+    {
+        int messageVector[3][1];
+        for (int j = 0; j < 3; j++)
+        {
+            messageVector[i][0] = (plaintext[i * 3 + j]) % 65;
+        }
+
+        int messageMatrix[3][1];
+        encrypt(messageMatrix, messageVector);
+
+        for (int i = 0; i < 3; i++)
+        {
+            cipherText += messageMatrix[i][0] + 65;
+        }
+    }
+
     return cipherText;
 }
 
-string hillCipherDecryption(const string cipherText) {
+string hillCipherDecryption(const string cipherText)
+{
     string plaintext;
+
+    for (int i = 0; i < cipherText.length() / 3; i++)
+    {
+        int cipherVector[3][1];
+        for (int j = 0; j < 3; j++)
+        {
+            cipherVector[i][0] = (cipherText[i * 3 + j]) % 65;
+        }
+
+        int cipherMatrix[3][1];
+        decrypt(cipherMatrix, cipherVector);
+
+        for (int i = 0; i < 3; i++)
+        {
+            plaintext += cipherMatrix[i][0] + 65;
+        }
+    }
+
     return plaintext;
 }
 
@@ -144,18 +246,20 @@ int main()
         if (choice == 3)
             break;
 
-        string plaintext, cipherText, decryptedText, key;
+        string plaintext, cipherText, decryptedText;
         switch (choice)
         {
         case 1:
-            key = readFile("key.txt");
+        {
             int invalidkey = keyMatricGeneration();
-            if(invalidkey) break;
+            if (invalidkey)
+                break;
             plaintext = readFile("file.txt");
             cipherText = hillCipherEncryption(plaintext);
             writeFile("Cipher.txt", cipherText);
             cout << "Encryption complete. Output written to Cipher.txt" << endl;
             break;
+        }
         case 2:
             cipherText = readFile("Cipher.txt");
             decryptedText = hillCipherDecryption(cipherText);
